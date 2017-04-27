a1613d7a8b2b767cd294e6456ba24ce421809073
Bryan LOT <bryan.lot@informatique.gov.pf>
2017-04-26 20:48:15 -1000

    fix syntax in sed command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -z "$ES_HOST" ]; then
-  sed -i 's|hosts = \[| hosts = [\\n {\\n    host = \"${ES_HOST}:${ES_PORT]\"\\n    name = \"${ES_NAME}\"\\n  ]|' ./conf/application.conf
+  sed -i "s|hosts = \[| hosts = [\\n {\\n    host = \"${ES_HOST}:${ES_PORT}\"\\n    name = \"${ES_NAME}\"\\n  }|" ./conf/application.conf
 fi
 
 gosu cerebro ./bin/cerebro
